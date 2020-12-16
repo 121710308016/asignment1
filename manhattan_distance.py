5f4e30813136d9df816e0db09ad2627e01d8e974
121710308016 <75672363+121710308016@users.noreply.github.com>
2020-12-16 11:13:10 +0530

    Add files via upload

---
+++ manhattan_distance.py
@@ -0,0 +1,39 @@
+import unittest
+
+# Implement the below function and run this file
+# Return the output, No need read input or print the ouput
+
+
+def manhattan_distance(x0, y0, x1, y1):
+    return abs(x1-x0) + abs(y1-y0)
+# DO NOT TOUCH THE BELOW CODE
+# pylint: disable=unused-variable
+class TestManhattanDistance(unittest.TestCase):
+
+    def test_01(self):
+        self.assertEqual(manhattan_distance(0, 1, 3, 2), 4)
+
+    def test_02(self):
+        self.assertEqual(manhattan_distance(3, 5, 6, 1), 7)
+
+    def test_03(self):
+        self.assertEqual(manhattan_distance(5, 5, 5, 5), 0)
+
+    def test_04(self):
+        self.assertEqual(manhattan_distance(24, 23, 25, 18), 6)
+
+    def test_05(self):
+        self.assertEqual(manhattan_distance(131, 333, 479, 297), 384)
+
+    def test_06(self):
+        self.assertEqual(manhattan_distance(145, 311, 261, 145), 282)
+
+    def test_07(self):
+        self.assertEqual(manhattan_distance(283, 861, 251, 806), 87)
+
+    def test_08(self):
+        self.assertEqual(manhattan_distance(583, 254, 564, 167), 106)
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
